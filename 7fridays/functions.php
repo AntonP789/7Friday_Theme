fe01e0f23e477f3c96b81955e05237ecef44d5d8
AntonP789 <61416733+AntonP789@users.noreply.github.com>
2021-05-12 15:11:52 +0300

    Fixed url uploading images

--- 7fridays/functions.php
+++ 7fridays/functions.php
@@ -176,3 +176,5 @@ add_action( 'wp_print_styles', 'wps_deregister_styles', 100 );
 function wps_deregister_styles() {
     wp_dequeue_style( 'wp-block-library' );
 }
+
+update_option('upload_url_path', '/wp-content/uploads'); 
