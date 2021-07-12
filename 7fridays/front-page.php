0fa4b01efe0d93a221b20c59b4a508227a9100aa
AntonP789 <61416733+AntonP789@users.noreply.github.com>
2021-07-12 09:36:16 +0300

    Update front-page.php
    change include path

--- 7fridays/front-page.php
+++ 7fridays/front-page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
 <div class="content">
-    <?php include(get_stylesheet_directory_uri().'/template-parts/main-flexible-content.php');?>
+    <?php include(get_theme_root().'/'.get_option('stylesheet').'/template-parts/main-flexible-content.php');?> 
 </div><!--End Content-->
 <?php get_footer(); ?>
